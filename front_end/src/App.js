23d8180199d9454a55db277be03fc0b9066ab79d
Qfront <97919196+Qfront@users.noreply.github.com>
2022-07-17 09:21:11 +0500

    moved first useState example to Counter.jsx

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,28 +2,12 @@ import React, {useState} from 'react';
 import Counter from "./components/Counter";
 
 function App() {
-    const [likes, setLikes] = useState(0)
     const [text, setText] = useState("Text")
-    function change_likes(action) {
-        switch(action) {
-            case "+":
-                setLikes(likes + 1);
-                return
-            case "-":
-                setLikes(likes - 1);
-                return
-            default:
-                console.log('none action');
-        }
-    }
   return (
     <div className="App">
-        <h1>{likes}</h1>
-        <button onClick={() => change_likes("+")}>+</button>
-        <button onClick={() => change_likes("-")}>-</button>
         <h1>{text}</h1>
         <input onChange={event => setText(event.target.value)} value={text}></input>
-        <Counter />
+        <Counter/>
     </div>
   );
 }
--- front_end/src/components/Counter.jsx
+++ front_end/src/components/Counter.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Counter = () => {
+    const [count, setCount] = useState(0);
+    function change_count(action) {
+        switch(action) {
+            case "+":
+                setCount(count + 1);
+                return
+            case "-":
+                setCount(count - 1);
+                return
+            default:
+                console.log('none action');
+        }
+    }
     return (
         <div>
-            HELLO
+            <h1>{count}</h1>
+            <button onClick={() => change_count("+")}>+</button>
+            <button onClick={() => change_count("-")}>-</button>
         </div>
     );
 };
